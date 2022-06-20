83104380b05f7dc7eb2729910cdae62c85305f0b
zdn2442 <zdn.2442.ok@gmail.com>
2022-06-20 14:07:46 +0700

    End

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,60 @@
-var email = document.forms['form']['email']
+var nama = document.getElementById('nama')
+var email = document.getElementById('email')
+var pesan = document.getElementById('pesan')
 
+var nama_error = document.getElementById('nama_error')
 var email_error = document.getElementById('email_error')
+var pesan_error = document.getElementById('pesan_error')
 
-email.addEventListener('textInput', email_Verify)
+var errorData = true;
+
+email.addEventListener('textInput', emailVerify);
+nama.addEventListener('textInput', namaVerify);
+pesan.addEventListener('textInput', pesanVerify);
 
 function validated() {
     if (email.value.length < 9) {
-        email.style.border = "1px solid red" ;
-        email_error.style.display = "block";
-        email.focus()
-        return false
+        email.style.border = '1px solid red';
+        email_error.style.display = 'block';
+        errorData = false;
+    }
+    if (nama.value.length < 4) {
+        nama.style.border = '1px solid red';
+        nama_error.style.display = 'block';
+        errorData = false;
+    }
+    if (pesan.value.length < 4) {
+        pesan.style.border = '1px solid red';
+        console.log("alhfakdsjbaerubigbierbgu");
+        pesan_error.style.display = 'block';
+        errorData = false;
+    }
+    if (errorData == false) {
+       return false;
     }
 }
-function email_Verify() {
-    if (email.value.length >= 8) {
-        email.style.border = "1px solid silver" ;
-        email_error.style.display = "none";
-        return true
+
+function emailVerify() {
+    if (email.value.length >= 9) {
+        email.style.border = '1px solid silver';
+        email_error.style.display = 'none';
+        errorData = true;
+        return true;
     }
-    
+}
+function namaVerify() {
+    if (nama.value.length >= 4) {
+        nama.style.border = '1px solid silver';
+        nama_error.style.display = 'none';
+        errorData = true;
+        return true;
+    }
+}
+function pesanVerify() {
+    if (pesan.value.length >= 4) {
+        pesan.style.border = '1px solid silver';
+        pesan_error.style.display = 'none';
+        errorData = true;
+        return true;
+    }
 }
